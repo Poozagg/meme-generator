286da1442d9d4d2b0de7bc1e656d1c3b3a127fa3
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-05 14:37:01 +0000

    navbar meme image path updated again. as netlify domain is not showing meme image on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <nav className="Navbar--section">
-        <img src="./public/images/troll-face.png" alt="troll-face" className="Navbar--image" />
+        <img src="/public/images/troll-face.png" alt="troll-face" className="Navbar--image" />
         <h2 className="Navbar--name"> Meme Generator </h2>
         <h4 className="Navbar--project"> React Course - Project 1 </h4>
     </nav>
