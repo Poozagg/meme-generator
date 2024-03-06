2f7ee3caa0c4f3df5d231e644044d920c364a1ed
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-06 15:36:44 +0000

    project-3 instead of 1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className="Navbar--section">
         <img src="/public/images/troll-face.png" alt="troll-face" className="Navbar--image" />
         <h2 className="Navbar--name"> Meme Generator </h2>
-        <h4 className="Navbar--project"> React Course - Project 1 </h4>
+        <h4 className="Navbar--project"> React Course - Project 3 </h4>
     </nav>
   )
 }
